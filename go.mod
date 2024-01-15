6048d60932024acf49f4a195f535d9608c55200a
davidjoliver86 <davidjoliver.2021@gmail.com>
2024-01-15 09:48:58 -0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/davidjoliver86/shiba
+
+go 1.21.5
---
+++ shiba.go
@@ -0,0 +1,7 @@
+package shiba
+
+import "strings"
+
+func WhenGrownUp(s string) string {
+	return "SHIBA TO THE MOON! " + strings.ToUpper(s)
+}
